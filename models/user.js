750b8f0985189d428beb52c88dea299adbbd0ce2
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-10-05 13:23:48 +0300

    validation

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -9,12 +11,16 @@ const userSchema = new mongoose.Schema({
   },
   about: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    required: [true, 'Поле "about" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "about" - 2'],
+    maxlength: [30, 'Максимальная длина поля "about" - 30'],
   },
   avatar: {
     type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
     required: true,
   },
 }, {
