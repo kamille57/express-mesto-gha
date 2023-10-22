af78b17a25b4327472ff607cdaad5a8cae8df805
kamille57 <126716411+kamille57@users.noreply.github.com>
2023-10-22 13:01:38 +0300

    joi

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate } = require('celebrate'); // добавляем валидацию через celebrate
+
 const {
   getUsers,
   getUser,
@@ -16,12 +18,12 @@ const {
 router.get('/users', getUsers);
 
 // Получить пользователя по _id
-router.get('/users/:userId', getUserByIdSchema, getUser);
+router.get('/users/:userId', celebrate(getUserByIdSchema), getUser); // используем celebrate для валидации
 
 // Обновить профиль
-router.patch('/users/me', updateUserProfileSchema, updateProfile);
+router.patch('/users/me', celebrate(updateUserProfileSchema), updateProfile); // используем celebrate для валидации
 
 // Обновить аватар
-router.patch('/users/me/avatar', updateUserAvatarSchema, updateAvatar);
+router.patch('/users/me/avatar', celebrate(updateUserAvatarSchema), updateAvatar); // используем celebrate для валидации
 
 module.exports = router;
